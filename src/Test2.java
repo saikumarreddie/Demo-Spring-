49b9705dcbc152fb5da7dac538832bebee18d4c2
saikumarreddie <saikumarreddybandi6@gmail.com>
2021-03-02 08:52:18 +0530

    completed HIS-122

--- src/Test2.java
+++ src/Test2.java
@@ -5,7 +5,14 @@ public class Test2 {
 		// TODO Auto-generated method stub
 
 		System.out.println("this is test2 class");
+		
+		
 
+	}
+	
+	public  static void start() {
+		
+		// Changes for HIS-122
 	}
 
 }
